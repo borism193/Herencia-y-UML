aef3dce2be087e6de76531b93bb2c9a1ac7887d5
boris <boris.miranda@gmail.com>
2021-04-25 20:32:13 -0400

    herencia final

--- herencia_uml.rb
+++ herencia_uml.rb
@@ -55,3 +55,9 @@ class OneTimeAppointment < Appointment
 end
 
 
+puts OneTimeAppointment.new('Desafío Latam', 'Trabajo', 14, 30, 4, 6, 2019)
+# Reunión única en Desafío Latam sobre Trabajo el 4/6/2019 a la(s) 14:30.
+puts DailyAppointment.new('Desafío Latam', 'Educación', 8, 15)
+# Reunión diaria en Desafío Latam sobre Educación a la(s) 8:15.
+puts MonthlyAppointment.new('NASA', 'Aliens', 8, 15, 23)
+# Reunión mensual en NASA sobre Aliens el día 23 a la(s) 8:15.
